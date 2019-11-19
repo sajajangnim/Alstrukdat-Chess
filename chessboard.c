8210bb76f33829b5552520258abf5a62a5534b53
yusufnoormuhammad <yusuf.muhammad0518@gmail.com>
2019-11-19 19:42:45 +0700

    Update chessboard.c

--- chessboard.c
+++ chessboard.c
@@ -6,74 +6,89 @@ char Player1[10];
 char Player2[10];
 
 
-void MakeBoard(ChessBoard *T) {
-    int i,j;
-    for (i = 1; i <= MaxRow; i++) {
-        switch (i) {
-            case (1): for (j = 1; j <= MaxCol; j++) {
-                                switch (j) {
-                                    case (1):
-                                    case (8): (*T).Board[i][j] = ROOK_B;
-                                    case (2):
-                                    case (7): (*T).Board[i][j] = KNIGHT_B;
-                                    case (3):
-                                    case (6): (*T).Board[i][j] = BISHOP_B;
-                                    case (4): (*T).Board[i][j] = QUEEN_B;
-                                    case (5): (*T).Board[i][j] = KING_B;
-                                }
-            }
-            case (2): for (j = 1; j <= MaxCol; j++) {
-                                (*T).Board[i][j] = PAWN_B;
-            }
-            case (7): for (j = 1; j <= MaxCol; j++) {
-                                (*T).Board[i][j] = PAWN_W;
-            }
-            case (8): for (j = 1; j <= MaxCol; j++) {
-                                switch (j) {
-                                    case (1):
-                                    case (8): (*T).Board[i][j] = ROOK_W; break;
-                                    case (2):
-                                    case (7): (*T).Board[i][j] = KNIGHT_W; break;
-                                    case (3):
-                                    case (6): (*T).Board[i][j] = BISHOP_W; break;
-                                    case (4): (*T).Board[i][j] = QUEEN_W; break;
-                                    case (5): (*T).Board[i][j] = KING_W; break;
-                                }
-            }
-            default: for (j = 1; j <= MaxCol; j++) {
-                                (*T).Board[i][j] = ' ';
-            }
-        }
-    }
+void MakeBoard(MATRIKS *M) {
+    Elmt(*M,1,8) = ROOK_B;
+    Elmt(*M,2,8) = KNIGHT_B;
+    Elmt(*M,3,8) = BISHOP_B;
+    Elmt(*M,4,8) = QUEEN_B;
+    Elmt(*M,5,8) = KING_B;
+    Elmt(*M,6,8) = BISHOP_B;
+    Elmt(*M,7,8) = KNIGHT_B;
+    Elmt(*M,8,8) = ROOK_B;
+    Elmt(*M,1,7) = PAWN_B;
+    Elmt(*M,2,7) = PAWN_B;
+    Elmt(*M,3,7) = PAWN_B;
+    Elmt(*M,4,7) = PAWN_B;
+    Elmt(*M,5,7) = PAWN_B;
+    Elmt(*M,6,7) = PAWN_B;
+    Elmt(*M,7,7) = PAWN_B;
+    Elmt(*M,8,7) = PAWN_B;
+
+    Elmt(*M,1,1) = ROOK_W;
+    Elmt(*M,2,1) = KNIGHT_W;
+    Elmt(*M,3,1) = BISHOP_W;
+    Elmt(*M,4,1) = QUEEN_W;
+    Elmt(*M,5,1) = KING_W;
+    Elmt(*M,6,1) = BISHOP_W;
+    Elmt(*M,7,1) = KNIGHT_W;
+    Elmt(*M,8,1) = ROOK_W;
+    Elmt(*M,1,2) = PAWN_W;
+    Elmt(*M,2,2) = PAWN_W;
+    Elmt(*M,3,2) = PAWN_W;
+    Elmt(*M,4,2) = PAWN_W;
+    Elmt(*M,5,2) = PAWN_W;
+    Elmt(*M,6,2) = PAWN_W;
+    Elmt(*M,7,2) = PAWN_W;
+    Elmt(*M,8,2) = PAWN_W;
 }
 
-void DisplayBoard(ChessBoard T) {
-    int i;
-    int j;
-    int n = 8;
-
-    printf("BLACK: %s", Player1);
-    for (i = 1; i <= 15; i++) {
-        printf(" ");
-    }
-    printf("white: %s\n", Player2);
-
-    for (i = 1; i <= 42; i++) {
-        printf("=");
-    }
-    printf("\n");
-    for (i = 1; i <= 8; i++) {
-        j = 1;
-        printf("|| ");
-        while (j <= (n)) {
-            printf("%c", T.Board[i-1][j-1]);
-            printf(" || ");
-            j++;
-        }
-        printf("\n");
-    }
-    for (i = 1; i <= 42; i++) {
-        printf("=");
-    }
-    printf("\n");
+void DisplayBoard(MATRIKS *M) {
+  for (int i = 1; i <= 39; i++) {
+    printf("=");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,8));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,7));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,6));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,5));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,4));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,3));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,2));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i<=8; i++) {
+    printf("%c",Elmt(*M,i,1));
+    printf(" || ");
+  }
+  printf("\n");
+  for (int i = 1; i <= 39; i++) {
+    printf("=");
+  }
+  printf("\n");
 }
